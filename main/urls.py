8777d647e096cd230e9c67789b3d0a116c3acd71
Alejandro Martinez <janusreal@gmail.com>
2024-06-06 01:07:49 -0400

    [update] Se agrega funciones de formulario de contacto

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from main.views import index, about, welcome
+from main.views import index, about, welcome, contact_form, exito
 
 urlpatterns = [
     path('', index, name="index"),
     path('about/', about, name="about"),
-    path('welcome/', welcome, name="welcome")
+    path('welcome/', welcome, name="welcome"),
+    path('contact_form', contact_form, name="contact_form"),
+    path('exito/', exito, name="exito")
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from main.flanes import flanes
 
@@ -28,4 +28,45 @@ def welcome(req):
         'esAdmin': True,
         'nombre': 'Alejandro Martínez',
     }
+    errores = req.session.pop('errores', None)
+    context = {'errores': errores}
     return render(req, "welcome.html", context )
+
+def exito(req):
+    return render(req, "exito.html")
+
+def contact_form(req):
+    customer_name = req.POST['customer_name']
+    customer_email = req.POST['customer_email']
+    message = req.POST['message']
+    
+    errores = []
+    
+    if not '@' in customer_email:
+        errores.append('Falta la arroba')
+            
+    if len(customer_name) >= 64:
+        errores.append('Nombre excede el largo de 64 caracteres')
+        
+    if customer_name == '':
+        errores.append("No has ingresado un nombre")
+    
+    if customer_email == '':
+        errores.append("No has ingresado un email")
+            
+    if len(errores) > 0:
+        context = {
+            'errores': errores,
+            'flag': True
+            }
+        return render(req, 'welcome.html', context)    
+    
+    return redirect('/exito')
+ 
+
+    
+    # ahora tengo que validar que 'customer_email' tenga al menos un @ y un .
+    # y que customer_name sea de largo maximo 64
+    
+    # si len(errores) == 0: redirijo a pagina de exito
+    # si len(errores) > 0: vuelvo a cargar 'welcome.html', pero ahora mostrando los eorrores 
