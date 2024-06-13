5cd5e1b273b25ba52ee2e2a7836eab600d476a31
Alejandro Martinez <janusreal@gmail.com>
2024-06-13 19:22:43 -0400

    [update] Gestión de botones de registro y login

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import index, about, contact, exito, welcome
+from main.views import index, about, contact, exito, welcome, register
 
 
 urlpatterns = [
@@ -8,4 +8,7 @@ urlpatterns = [
     path('welcome/', welcome, name="welcome"),
     path('contact/', contact, name="contact"),
     path('exito/', exito, name="exito"),
+    path('register', register)
 ]
+
+# name="exito" es una ruta nombrada
--- main/views.py
+++ main/views.py
@@ -3,11 +3,9 @@ from django.http import HttpResponse
 from main.flanes import flanes
 from main.forms import ContactForm
 from main.models import Contacto, Flan
+from django.contrib.auth.decorators import login_required
 
 #create views
-
-
-
 
 def about(req):
     context = {
@@ -42,11 +40,6 @@ def contact(req):
             Contacto.objects.create(
                 **form.cleaned_data
             )
-            '''Contactos.objects.create({
-                'nombre':form.nombre,
-                'email': form.email,
-                'edad':form.edad
-            })'''
             return redirect('/exito')
         context = {'form': form }
         return render(req,'contact.html',context)
@@ -90,10 +83,52 @@ def contact_form(req):
     # si len(errores) == 0: redirijo a pagina de exito
     # si len(errores) > 0: vuelvo a cargar 'welcome.html', pero ahora mostrando los eorrores 
 
+
+@login_required
 def welcome(req):
-    all_flanes = Flan.objects.filter(is_private=True)
+    all_flanes = Flan.objects.all()
     return render(req, 'welcome.html', {'all_flanes': all_flanes})
 
 def index(req):
     all_flanes = Flan.objects.filter(is_private=False)
     return render(req, 'index.html', {'all_flanes': all_flanes})
+
+def login(req):
+    return render(req, 'login.html')  
+
+def register_form(req):
+    
+    username = req.POST['username']
+    password1 = req.POST['password1']
+    password2 = req.POST['password2']
+
+    errores = []
+    
+    if password1 != password2:
+        errores.append('Las contraseñas no coinciden')
+                
+    if len(password1) < 8:
+        errores.append('La contraseña debe contener 8 caracteres como mínimo')
+    
+    if len(username) >= 15:
+        errores.append('El nombre de usuario no debe exceder los 15 caracteres')
+        
+    if username == '':
+        errores.append("No has ingresado un nombre de usuario")
+    
+    if password1 == '':
+        errores.append("No has registrado una contraseña")
+            
+    if len(errores) > 0:
+        context = {
+            'errores': errores
+            }
+        return render(req, 'register.html', context)    
+    
+    return redirect('/exito')
+
+
+
+def register(req):
+    return render(req, 'register.html')  
+   
